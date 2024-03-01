d8e55191f0e2f4bd6d328f873384ed0db7b14510
Mojidul <mojidul31@gmail.com>
2024-03-02 00:45:34 +0600

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -18,165 +18,171 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'Roboto'),
       home: Scaffold(
-        backgroundColor: Colors.white,
-        //backgroundColor: Colors.pink[400],
-        //backgroundColor: Color(0xFFF06292),
-        /*
-        body: Center(child: Text('Body Text',
-          style: TextStyle(
-            fontFamily: 'Roboto',
-              fontSize:20.0,
-              fontWeight: FontWeight.normal,
-              //fontStyle: FontStyle.italic,
-              color: Colors.green
-          ),
-          textAlign: TextAlign.center,
-        )
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Color(0xFFAD1454),
+          title: Text('Calculator',
+          style: TextStyle(fontSize: 36.0, color: Colors.white, fontWeight: FontWeight.bold),),
         ),
-        */
-        /*
-        body:Center(
-          child: Icon(
-            Icons.favorite,
-            //Icons.android,
-            color:Color(0xFFF06292),
-            size:400.0
-          ),
-        )
-        */
-        /*
-        body: Container(
-          child: Text('Body Text',
-                style: TextStyle(
-                fontSize:30.0,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                color: Colors.green
-            ),
-          ),
-          decoration: BoxDecoration(
-            color: Colors.pink,
-            shape: BoxShape.rectangle
-          ),
-          margin: EdgeInsets.all(50.0),
-          //padding: EdgeInsets.symmetric(vertical: 10.0),
-          padding: EdgeInsets.symmetric(vertical:5.0, horizontal: 10.0),
-          alignment: Alignment.center,
-          height: 180.0,
-          width: 180.0,
-        )
-        */
-        //here we remember that Column and Row behaviour are almost same
-        /*
-          body: Column(
-          children: [
-            Container(
-              child: Text('Container One',
-                style: TextStyle(
-                    fontSize:30.0,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.green
-                ),
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.pink,
-                  shape: BoxShape.rectangle
-              ),
-              margin: EdgeInsets.only(top: 40.0, left: 20.0),
-              alignment: Alignment.center,
-              height: 180.0,
-              width: 180.0,
-            ),
-            SizedBox(height: 20.0,),
-            Container(
-              child: Text('Container Two',
-                style: TextStyle(
-                    fontSize:30.0,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.green
-                ),
-              ),
-              decoration: BoxDecoration(
-                  color: Colors.pink,
-                  shape: BoxShape.rectangle
-              ),
-              margin: EdgeInsets.only(left: 20.0),
-              alignment: Alignment.center,
-              height: 180.0,
-              width: 180.0,
-            )
-          ],
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-        */
-        /*
-        body:Column(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
             Expanded(
               flex: 2,
-              child: Container(
-                child:Text('1',
-                style: TextStyle(fontSize: 30.0, color: Colors.white),
-                ),
-                color: Colors.green,
-                alignment: Alignment.center,
-                //width: 100.0,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Text('1', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Colors.green,
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                ],
               ),
             ),
             Expanded(
               flex: 1,
-              child: Container(
-                child:Text('2',
-                  style: TextStyle(fontSize: 30.0, color: Colors.white),
-                ),
-                color: Colors.pink,
-                alignment: Alignment.center,
-                //width: 100.0,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Text('1', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFAD1457),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('2', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFC2185B),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('3', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFD81B60),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('+', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFE91E63),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                ],
               ),
             ),
             Expanded(
               flex: 1,
-              child: Container(
-                child:Text('3',
-                  style: TextStyle(fontSize: 30.0, color: Colors.white),
-                ),
-                color: Colors.blue,
-                alignment: Alignment.center,
-                //width: 100.0,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Text('4', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFE91E63),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('5', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFD81B60),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('6', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFC2185B),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Icon(Icons.remove, color: Colors.white, size: 24.0,),
+                      color: Color(0xFFAD1457),
+                      alignment: Alignment.center,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Text('7', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFAD1457),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('8', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFC2185B),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('9', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFD81B60),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Icon(Icons.clear, color: Colors.white, size: 24.0,),
+                      color: Color(0xFFE91E63),
+                      alignment: Alignment.center,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Text('0', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFE91E63),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('C', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFD81B60),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('=', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFC2185B),
+                      alignment: Alignment.center,
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      child: Text('/', style: TextStyle(fontSize: 36.0, color: Colors.white),),
+                      color: Color(0xFFAD1457),
+                      alignment: Alignment.center,
+                    ),
+                  )
+                ],
               ),
             )
           ],
-        )
-        */
-          body:Column(
-            children: [
-              Expanded(
-                  flex: 2,
-                  child: InkWell(
-                    onTap: (){
-
-                      setState((){
-                        counter += 1;
-                        print("Press Container, $counter");
-                      });
-
-                    },
-                    child: Container(
-                      child:Text(counter.toString(),
-                        style: TextStyle(fontSize: 30.0, color: Colors.white),
-                      ),
-                      color: Colors.green,
-                      alignment: Alignment.center,
-                      //width: 100.0,
-                    ),
-                  ),
-                ),
-
-            ],
-          )
+        ),
       )
     );
   }
